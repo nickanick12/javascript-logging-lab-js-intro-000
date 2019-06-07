ba88c7bc7a6616ea10eca48e0665c63c32284e5d
Nickalus Brewer <nickanick12@gmail.com>
2019-06-07 10:43:39 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
 index calls console.error("HALP!")
-console.log("I would be a logger.")
-console.warn("You are warned")
+index calls console.log("I would be a logger.")
+index calls console.warn("You are warned")
